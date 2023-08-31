5ab1f39e2c6281b8c030ef9d634404a4af46c80a
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-08-31 16:37:30 -0300

    adiciona o logo

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,33 @@
 import './globals.css'
+import styles from './styles.module.css'
+// import { Quicksand } from 'next/font/google'
 
 export const metadata = {
   title: 'Travel Landing Page',
 }
 
+// const quicksand = Quicksand({
+//   subsets: ['latin'],
+//   display: 'swap',
+// })
+
+function Logo(){
+  return (
+    <p className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+  );
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <Logo/>
+        </header>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
