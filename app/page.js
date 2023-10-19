2befc76ad4422127febd43c4ab8a59926350e8e8
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-10-19 14:20:59 -0300

    adiciona o botão Start

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 import styles from './page.module.css'
 
+function Botao({x, y}) {
+  return (
+    <button style={{left: x, top: y}} className={styles.botao}>Start</button>
+  )
+}
+
 export default function Home() {
   return (
-    <h1>Teste</h1>
+    <Botao x={135} y={601.72}/>
   )
 }
