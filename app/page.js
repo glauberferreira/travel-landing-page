6e4f2e768479b76ca9539e69432a73ae5b59402a
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-10-19 16:30:34 -0300

    versão inicial dos cards

--- app/page.js
+++ app/page.js
@@ -1,4 +1,9 @@
 import styles from './page.module.css'
+import Image from 'next/image'
+import bali from '../public/bali.png'
+import cyprus from '../public/cyprus.png'
+import hawaii from '../public/hawaii.png'
+import seta from '../public/arrow-right.svg'
 
 function Botao({x, y}) {
   return (
@@ -6,8 +11,23 @@ function Botao({x, y}) {
   )
 }
 
+function Card({localidade, imagem}) {
+  return(
+    <div className={styles.card}>
+        <p className={styles.localidade}>{localidade}</p>
+        <Image src={seta}/>
+        <Image src={imagem}/>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
-    <Botao x={135} y={601.72}/>
+    <>
+      <Botao x={135} y={601.72}/>
+      <Card localidade="Hawaii" imagem={hawaii}/>
+      <Card localidade="Cyprus" imagem={cyprus}/>
+      <Card localidade="Bali" imagem={bali}/>
+    </>
   )
 }
